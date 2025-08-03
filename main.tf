4e37b93903f4be57a10563057eaff39a8bf79329
Oscar Eduardo Mayor Jaramillo <oscar.mayor.dev@gmail.com>
2025-08-03 17:25:57 +1200

    Update main.tf
    se edito porque la version free no permite t3.micro

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "us-east-1" # Asegúrate que tu región permite t2.micro
+}
+
 data "aws_ami" "app_ami" {
   most_recent = true
 
@@ -16,7 +20,7 @@ data "aws_ami" "app_ami" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t2.micro"
+  instance_type = "t2.micro" # O usa "t3.micro" si es necesario
 
   tags = {
     Name = "HelloWorld"
